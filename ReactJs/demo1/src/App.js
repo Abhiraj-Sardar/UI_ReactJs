6937dcb5879c7883674c118bb8a22d2d5ffc2cd5
Abhiraj <abhirajsardar2003@gmail.com>
2024-01-10 22:09:28 +0530

    Day 1 Task Complete

---
+++ ReactJs/demo1/src/App.js
@@ -0,0 +1,420 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Paragraph1/>
+      <Paragraph2/>
+      <Paragraph3/>
+      <Paragraph4/>
+      <Paragraph5/>
+      <Paragraph6/>
+      <Paragraph7/>
+      <Paragraph8/>
+      <Paragraph9/>
+      <Paragraph30/>
+      <hr/>
+      <h1>Personal Information</h1>
+      <Name/>
+      <Roll/>
+      <College/>
+      <City/>
+      <State/>
+      <hr/>
+      <h1>Student Record</h1>
+      <Table/>
+      <hr/>
+      <h1>Simple Form</h1>
+      <Form/>
+      <hr/>
+      <h1>Rules & Regulations</h1>
+      <Rules/>
+      <hr/>
+      <h1>Notice</h1>
+      <Notice/>
+    </div>
+  );
+}
+
+function Header(){
+
+    return(
+      <h1>WIKIPEDIA</h1>
+    );
+}
+
+function Paragraph1(){
+  return (
+    <p>Learn about Wikidata
+
+  What is Wikidata? Read the Wikidata introduction.
+  Explore Wikidata by looking at a featured showcase item for author Douglas Adams (Q42).
+  Get started with Wikidata's SPARQL query service.
+  Contribute to Wikidata
+  
+  Learn to edit Wikidata: follow the tutorials.
+  Work with other volunteers on a subject that interests you: join a WikiProject.
+  Individuals and organizations can also donate data.
+  Meet the Wikidata community
+  
+  Visit the community portal or attend a Wikidata event.
+  Create a user account.
+  Talk and ask questions on the Project chat, Telegram groups, or the live IRC chatconnect.
+  Use data from Wikidata
+  
+  Learn how you can retrieve and use data from Wikidata.</p>
+  )
+  
+}
+function Paragraph2(){
+  return (
+    <p>Learn about Wikidata
+
+  What is Wikidata? Read the Wikidata introduction.
+  Explore Wikidata by looking at a featured showcase item for author Douglas Adams (Q42).
+  Get started with Wikidata's SPARQL query service.
+  Contribute to Wikidata
+  
+  Learn to edit Wikidata: follow the tutorials.
+  Work with other volunteers on a subject that interests you: join a WikiProject.
+  Individuals and organizations can also donate data.
+  Meet the Wikidata community
+  
+  Visit the community portal or attend a Wikidata event.
+  Create a user account.
+  Talk and ask questions on the Project chat, Telegram groups, or the live IRC chatconnect.
+  Use data from Wikidata
+  
+  Learn how you can retrieve and use data from Wikidata.</p>
+  )
+  
+}
+function Paragraph3(){
+  return (
+    <p>Learn about Wikidata
+
+  What is Wikidata? Read the Wikidata introduction.
+  Explore Wikidata by looking at a featured showcase item for author Douglas Adams (Q42).
+  Get started with Wikidata's SPARQL query service.
+  Contribute to Wikidata
+  
+  Learn to edit Wikidata: follow the tutorials.
+  Work with other volunteers on a subject that interests you: join a WikiProject.
+  Individuals and organizations can also donate data.
+  Meet the Wikidata community
+  
+  Visit the community portal or attend a Wikidata event.
+  Create a user account.
+  Talk and ask questions on the Project chat, Telegram groups, or the live IRC chatconnect.
+  Use data from Wikidata
+  
+  Learn how you can retrieve and use data from Wikidata.</p>
+  )
+  
+}
+function Paragraph4(){
+  return (
+    <p>Learn about Wikidata
+
+  What is Wikidata? Read the Wikidata introduction.
+  Explore Wikidata by looking at a featured showcase item for author Douglas Adams (Q42).
+  Get started with Wikidata's SPARQL query service.
+  Contribute to Wikidata
+  
+  Learn to edit Wikidata: follow the tutorials.
+  Work with other volunteers on a subject that interests you: join a WikiProject.
+  Individuals and organizations can also donate data.
+  Meet the Wikidata community
+  
+  Visit the community portal or attend a Wikidata event.
+  Create a user account.
+  Talk and ask questions on the Project chat, Telegram groups, or the live IRC chatconnect.
+  Use data from Wikidata
+  
+  Learn how you can retrieve and use data from Wikidata.</p>
+  )
+  
+}
+function Paragraph5(){
+  return (
+    <p>Learn about Wikidata
+
+  What is Wikidata? Read the Wikidata introduction.
+  Explore Wikidata by looking at a featured showcase item for author Douglas Adams (Q42).
+  Get started with Wikidata's SPARQL query service.
+  Contribute to Wikidata
+  
+  Learn to edit Wikidata: follow the tutorials.
+  Work with other volunteers on a subject that interests you: join a WikiProject.
+  Individuals and organizations can also donate data.
+  Meet the Wikidata community
+  
+  Visit the community portal or attend a Wikidata event.
+  Create a user account.
+  Talk and ask questions on the Project chat, Telegram groups, or the live IRC chatconnect.
+  Use data from Wikidata
+  
+  Learn how you can retrieve and use data from Wikidata.</p>
+  )
+  
+}
+function Paragraph6(){
+  return (
+    <p>Learn about Wikidata
+
+  What is Wikidata? Read the Wikidata introduction.
+  Explore Wikidata by looking at a featured showcase item for author Douglas Adams (Q42).
+  Get started with Wikidata's SPARQL query service.
+  Contribute to Wikidata
+  
+  Learn to edit Wikidata: follow the tutorials.
+  Work with other volunteers on a subject that interests you: join a WikiProject.
+  Individuals and organizations can also donate data.
+  Meet the Wikidata community
+  
+  Visit the community portal or attend a Wikidata event.
+  Create a user account.
+  Talk and ask questions on the Project chat, Telegram groups, or the live IRC chatconnect.
+  Use data from Wikidata
+  
+  Learn how you can retrieve and use data from Wikidata.</p>
+  )
+  
+}
+function Paragraph7(){
+  return (
+    <p>Learn about Wikidata
+
+  What is Wikidata? Read the Wikidata introduction.
+  Explore Wikidata by looking at a featured showcase item for author Douglas Adams (Q42).
+  Get started with Wikidata's SPARQL query service.
+  Contribute to Wikidata
+  
+  Learn to edit Wikidata: follow the tutorials.
+  Work with other volunteers on a subject that interests you: join a WikiProject.
+  Individuals and organizations can also donate data.
+  Meet the Wikidata community
+  
+  Visit the community portal or attend a Wikidata event.
+  Create a user account.
+  Talk and ask questions on the Project chat, Telegram groups, or the live IRC chatconnect.
+  Use data from Wikidata
+  
+  Learn how you can retrieve and use data from Wikidata.</p>
+  )
+  
+}
+function Paragraph8(){
+  return (
+    <p>Learn about Wikidata
+
+  What is Wikidata? Read the Wikidata introduction.
+  Explore Wikidata by looking at a featured showcase item for author Douglas Adams (Q42).
+  Get started with Wikidata's SPARQL query service.
+  Contribute to Wikidata
+  
+  Learn to edit Wikidata: follow the tutorials.
+  Work with other volunteers on a subject that interests you: join a WikiProject.
+  Individuals and organizations can also donate data.
+  Meet the Wikidata community
+  
+  Visit the community portal or attend a Wikidata event.
+  Create a user account.
+  Talk and ask questions on the Project chat, Telegram groups, or the live IRC chatconnect.
+  Use data from Wikidata
+  
+  Learn how you can retrieve and use data from Wikidata.</p>
+  )
+  
+}
+function Paragraph9(){
+  return (
+    <p>Learn about Wikidata
+
+  What is Wikidata? Read the Wikidata introduction.
+  Explore Wikidata by looking at a featured showcase item for author Douglas Adams (Q42).
+  Get started with Wikidata's SPARQL query service.
+  Contribute to Wikidata
+  
+  Learn to edit Wikidata: follow the tutorials.
+  Work with other volunteers on a subject that interests you: join a WikiProject.
+  Individuals and organizations can also donate data.
+  Meet the Wikidata community
+  
+  Visit the community portal or attend a Wikidata event.
+  Create a user account.
+  Talk and ask questions on the Project chat, Telegram groups, or the live IRC chatconnect.
+  Use data from Wikidata
+  
+  Learn how you can retrieve and use data from Wikidata.</p>
+  )
+  
+}
+function Paragraph30(){
+  return (
+    <p>Learn about Wikidata
+
+  What is Wikidata? Read the Wikidata introduction.
+  Explore Wikidata by looking at a featured showcase item for author Douglas Adams (Q42).
+  Get started with Wikidata's SPARQL query service.
+  Contribute to Wikidata
+  
+  Learn to edit Wikidata: follow the tutorials.
+  Work with other volunteers on a subject that interests you: join a WikiProject.
+  Individuals and organizations can also donate data.
+  Meet the Wikidata community
+  
+  Visit the community portal or attend a Wikidata event.
+  Create a user account.
+  Talk and ask questions on the Project chat, Telegram groups, or the live IRC chatconnect.
+  Use data from Wikidata
+  
+  Learn how you can retrieve and use data from Wikidata.</p>
+  )
+  
+}
+
+function Name(){
+return(
+  <h3>Name : Abhiraj Sardar</h3>
+)
+}
+
+function Roll(){
+  return (
+    <h3>Roll : 33003223027</h3>
+  )
+}
+
+function City(){
+  return (
+    <h3>City : Kolkata</h3>
+  )
+}
+
+function State(){
+  return(
+    <h3>State : West Bengal</h3>
+  )
+}
+
+function College(){
+  return(
+    <h3>College : Meghnad Saha Institute of Technology</h3>
+  )
+}
+
+function Table(){
+  return(
+    <table border="3">
+      <thead>
+        <tr>
+          <th>Roll</th>
+          <th>Name</th>
+          <th>Class</th>
+          <th>age</th>
+        </tr>
+      </thead>
+      <tbody>
+      <tr>
+            <td>1</td>
+            <td>John</td>
+            <td>10</td>
+            <td>15</td>
+        </tr>
+        <tr>
+            <td>2</td>
+            <td>Emma</td>
+            <td>10</td>
+            <td>16</td>
+        </tr>
+        <tr>
+            <td>3</td>
+            <td>Michael</td>
+            <td>10</td>
+            <td>15</td>
+        </tr>
+        <tr>
+            <td>4</td>
+            <td>Sarah</td>
+            <td>10</td>
+            <td>15</td>
+        </tr>
+        <tr>
+            <td>5</td>
+            <td>David</td>
+            <td>10</td>
+            <td>16</td>
+        </tr>
+        <tr>
+            <td>6</td>
+            <td>Emily</td>
+            <td>10</td>
+            <td>15</td>
+        </tr>
+        <tr>
+            <td>7</td>
+            <td>James</td>
+            <td>10</td>
+            <td>16</td>
+        </tr>
+        <tr>
+            <td>8</td>
+            <td>Olivia</td>
+            <td>10</td>
+            <td>15</td>
+        </tr>
+        <tr>
+            <td>9</td>
+            <td>William</td>
+            <td>10</td>
+            <td>16</td>
+        </tr>
+        <tr>
+            <td>10</td>
+            <td>Isabella</td>
+            <td>10</td>
+            <td>15</td>
+        </tr>
+      </tbody>
+    </table>
+  )
+  
+}
+
+function Form(){
+  return (
+    <form> 
+      <label for="fname">First name:</label><br/>
+      <input type="text" id="fname" name="fname"/><br/>
+      <label for="lname">Last name:</label><br/>
+      <input type="text" id="lname" name="lname"/><br/><br/>
+      <input type="submit" value="Submit"/>
+    </form>
+  )
+}
+
+function Rules(){
+  return (
+    <div>
+    <ol type="1">
+      <li>Safety First: All members must prioritize safety in all activities. Any activity deemed unsafe should be reported to the management immediately.</li>
+      <li>Respect for Others: All members must treat each other with respect and dignity. Harassment or discrimination of any kind will not be tolerated.</li>
+      <li>Honesty and Integrity: All members are expected to act with honesty and integrity in all dealings, both internal and external.</li>
+      <li>Confidentiality: All members must respect and maintain the confidentiality of sensitive information.</li>
+      <li>Conflict of Interest: Members must avoid situations where personal interests could conflict with their responsibilities.</li>
+    </ol>
+    </div>
+  )
+}
+
+function Notice(){
+  return(
+    <div>
+      <marquee>!!! Exam Starts From 22nd Jan,2024 !!! Admit Card will be Released on 15th Jan,2024 !!! Exam Fee 2000/- !!! </marquee>
+    </div>
+  )
+}
+
+export default App;
